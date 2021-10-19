188008e7be5e94fdfaaf20c09de608e27a1252b6
gusio11gk12 <91569176+gusio11gk12@users.noreply.github.com>
2021-10-19 17:54:18 -0500

    problema para mayor-menor de edad
    problema visto en clase para saber si eres mayor o menor de edad

---
+++ problemamayor.c
@@ -0,0 +1,28 @@
+/*
+ Autor: Gustavo Garcia Barrera
+ */
+
+/*Realizar un código que determine si el usuario esmayor o menor de edad dependiendo del año de nacimiento
+ 
+#include <stdio.h>
+
+int main(){//Inicio metodo principal
+ 
+        //Declarcion de variables
+	int fecha_nacimiento;
+	int fecha_actual;
+	int diferencia= 0;
+
+	printf ("Digite su año de nacimiento; ")
+	scanf ("%d", &fecha_Nacimiento);
+
+	diferencia = (fecha_Actual - fecha_Nacimineto);
+	
+	if (diferencia >= 18){
+	           printf("%s %d %s", "El usuario tiene", diferencia, "por lo tanto es mayor de edad");
+	 }
+        else {
+	           printf("%s %d %s", "El usuario tiene", diferencia, "por lo que es menor de edad");
+	}
+}//fin metodo principal
+
